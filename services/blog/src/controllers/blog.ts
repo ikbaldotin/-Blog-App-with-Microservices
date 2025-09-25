52ab34a61d5bb64237ba8d4486c27ca12842adc0
Md Ikbal <ikbal84336@gmail.com>
2025-09-26 00:16:33 +0530

    single blog

--- services/blog/src/controllers/blog.ts
+++ services/blog/src/controllers/blog.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { sql } from "../utils/db.js";
 import TryCatch from "../utils/trycatch.js";
 
@@ -18,4 +19,12 @@ export const getAllBlog = TryCatch(async (req, res) => {
     blogs = await sql`SELECT * FROM blog ORDER BY create_at DESC`;
   }
   res.status(200).json(blogs);
+});
+
+export const getSingleBlog = TryCatch(async (req, res) => {
+  const blog = await sql`SELECT * FROM blog WHERE id = ${req.params.id}`;
+  const { data } = await axios.get(
+    `${process.env.USER_SERVICE}/api/v1/user/${blog[0]?.author}`
+  );
+  res.json({ blog: blog[0], author: data });
 });
--- services/blog/src/routes/blog.ts
+++ services/blog/src/routes/blog.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { getAllBlog } from "../controllers/blog.js";
+import { getAllBlog, getSingleBlog } from "../controllers/blog.js";
 const route = express.Router();
 route.get("/blog/all", getAllBlog);
+route.get("/blog/:id", getSingleBlog);
 export default route;
